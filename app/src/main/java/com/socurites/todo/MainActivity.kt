ab750758f3abe2a9902ca12834c6d0b43dfdbcb7
Aaron Song <aaron@kmong.com>
2024-06-14 19:16:18 +0900

    configure: 프로젝트 구성

--- app/src/main/java/com/socurites/todo/MainActivity.kt
+++ app/src/main/java/com/socurites/todo/MainActivity.kt
@@ -3,13 +3,27 @@ package com.socurites.todo
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.material3.Button
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.socurites.todo.ui.theme.ToDoAppTheme
 
 class MainActivity : ComponentActivity() {
@@ -22,7 +36,7 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    Greeting("Android")
+                    TopLevel()
                 }
             }
         }
@@ -30,17 +44,74 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
+fun TopLevel() {
+
 }
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun ToDoInput(
+    text: String,
+    onTextChange: (String) -> Unit,
+    onSubmit: (String) -> Unit,
+) {
+    Row(modifier = Modifier.padding(8.dp)) {
+        OutlinedTextField(
+            value = text,
+            onValueChange = onTextChange,
+            modifier = Modifier.weight(1f))
+        Spacer(modifier = Modifier.size(8.dp))
+        Button(onClick = {
+            onSubmit(text)
+        }) {
+            Text(text = "입력")
+        }
+    }
+}
+
+@Composable
+fun ToDo(
+    todoData: ToDoData,
+    onEdit: (key: Int, text: String) -> Unit = {_, _ -> },
+    onToggle: (key: Int, text: String) -> Unit = {_, _ -> },
+    onDelete: (key: Int) -> Unit = { },
+) {
+    var isEditing by remember { mutableStateOf(false) }
+    Card(
+        modifier = Modifier.padding(4.dp),
+        elevation = CardDefaults.cardElevation(defaultElevation = 8.dp),
+    ) {
+
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun ToDoInputPreview() {
+    ToDoAppTheme {
+        ToDoInput(text = "테스트", onTextChange = {}, onSubmit = {})
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun ToDoPreview() {
+    ToDoAppTheme {
+        ToDo(todoData = ToDoData(1, "nice", true))
+    }
+}
+
 
 @Preview(showBackground = true)
 @Composable
 fun GreetingPreview() {
     ToDoAppTheme {
-        Greeting("Android")
+        TopLevel()
     }
 }
+
+data class ToDoData(
+    val key: Int,
+    val text: String,
+    val done: Boolean = false,
+)
