88b3bd4f92db52442f68b5ed349f544a9ca20a3e
Aaron Song <aaron@kmong.com>
2024-06-14 19:32:06 +0900

    feat: 기본 화면 구성

--- app/src/main/java/com/socurites/todo/MainActivity.kt
+++ app/src/main/java/com/socurites/todo/MainActivity.kt
@@ -3,6 +3,7 @@ package com.socurites.todo
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
@@ -11,9 +12,11 @@ import androidx.compose.foundation.layout.size
 import androidx.compose.material3.Button
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.Checkbox
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
@@ -21,6 +24,7 @@ import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -43,9 +47,19 @@ class MainActivity : ComponentActivity() {
     }
 }
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun TopLevel() {
+    val (text, setText) = remember { mutableStateOf("") }
+    val toDoList = remember { mutableListOf<ToDoData>() }
 
+    Scaffold {
+        Column(
+            modifier = Modifier.fillMaxSize().padding(it)
+        ) {
+            ToDoInput(text = text, onTextChange = setText, onSubmit = {})
+        }
+    }
 }
 
 @OptIn(ExperimentalMaterial3Api::class)
@@ -73,7 +87,7 @@ fun ToDoInput(
 fun ToDo(
     todoData: ToDoData,
     onEdit: (key: Int, text: String) -> Unit = {_, _ -> },
-    onToggle: (key: Int, text: String) -> Unit = {_, _ -> },
+    onToggle: (key: Int, checked: Boolean) -> Unit = {_, _ -> },
     onDelete: (key: Int) -> Unit = { },
 ) {
     var isEditing by remember { mutableStateOf(false) }
@@ -81,7 +95,27 @@ fun ToDo(
         modifier = Modifier.padding(4.dp),
         elevation = CardDefaults.cardElevation(defaultElevation = 8.dp),
     ) {
-
+        Row(
+            verticalAlignment = Alignment.CenterVertically
+        ) {
+            Text(
+                text = todoData.text,
+                modifier = Modifier.weight(1f),
+            )
+            Text(
+                text = "완료",
+            )
+            Checkbox(
+                checked = todoData.done,
+                onCheckedChange = { checked -> onToggle(todoData.key, checked)})
+            Button(onClick = { /*TODO*/ }) {
+                Text(text = "수정")
+            }
+            Spacer(modifier = Modifier.size(10.dp))
+            Button(onClick = { /*TODO*/ }) {
+                Text(text = "삭제")
+            }
+        }
     }
 }
 
