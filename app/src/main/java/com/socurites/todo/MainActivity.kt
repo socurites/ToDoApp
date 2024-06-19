7d557b8a1a8aaf5b9bbb2aa5cd3f933a42362c8c
Aaron Song <aaron@kmong.com>
2024-06-19 18:40:38 +0900

    refactor: 코드 정리

--- app/src/main/java/com/socurites/todo/MainActivity.kt
+++ app/src/main/java/com/socurites/todo/MainActivity.kt
@@ -3,7 +3,6 @@ package com.socurites.todo
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.activity.viewModels
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
@@ -19,16 +18,13 @@ import androidx.navigation.NavHostController
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
+import com.socurites.todo.screen.GithubReposScreen
 import com.socurites.todo.screen.TodoScreen
 import com.socurites.todo.ui.theme.ToDoAppTheme
-import com.socurites.todo.vm.github.GithubViewModel
 import dagger.hilt.android.AndroidEntryPoint
-import javax.inject.Inject
 
 @AndroidEntryPoint
 class MainActivity : ComponentActivity() {
-//    @Inject lateinit var vm: GithubViewModel by viewModels()
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
@@ -83,7 +79,7 @@ fun ToDoNav(
                             .fillMaxSize()
                             .padding(it)
                     ) {
-//                        GithubReposScreen()
+                        GithubReposScreen()
                     }
                 }
 
